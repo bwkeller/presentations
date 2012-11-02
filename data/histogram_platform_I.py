1bf4ab3407c29a6ea1705a00723588336d1b4872
Ben Keller <malzraa@gmail.com>
2012-11-01 22:56:19 -0400

    Removed the stuff from wrapup 1

--- data/histogram_platform_I.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import matplotlib.pyplot as plt
-
-if __name__ == "__main__":
-	I = [float(line.split()[0]) for line in open('platform_I.dat').readlines()]
-	plt.plot(I)
-	plt.xlabel("Moment of Inertia")
-	plt.show()
